fd5c65c781161a37d62e373c8d222bd9cdf83d61
Alexandr Shaposhnik <bromodragonfly11@gmail.com>
2022-03-30 17:02:06 +0300

    add model

--- vendor/ishop/core/Router.php
+++ vendor/ishop/core/Router.php
@@ -26,7 +26,9 @@ class Router
 
 	public static function dispatch($url)
 	{
-		if (self::matchRoute($url)){
+		$url = self::removeQueryString($url);
+
+ 		if (self::matchRoute($url)){
 			$controller = 'app\controllers\\'. self::$route['prefix ']
 				. self::$route['controller'] . 'Controller';
 
@@ -86,5 +88,17 @@ class Router
 	protected static function lowerCamelCase($name)
 	{
 		return lcfirst(self::upperCamelCase($name));
+	}
+
+	protected static function removeQueryString($url)
+	{
+		if ($url){
+			$params = explode("&", $url, 2);
+			if (false === strpos($params[0], '=')){
+				return rtrim($params[0], '/');
+			} else{
+				return  '';
+			}
+		}
 	}
 }
