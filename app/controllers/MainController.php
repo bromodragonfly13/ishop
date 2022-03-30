432c8c00fd292c94c0c4eb9d1c205eb8fd998c3d
Alexandr Shaposhnik <bromodragonfly11@gmail.com>
2022-03-30 20:00:02 +0300

    add template

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -14,18 +14,7 @@ class MainController extends AppController
 
 	public function indexAction()
 	{
-		$posts = \R::findAll('test');
 		$this->setMeta(App::$app->getProperty('shop_name'), 'Описание...', 'Ключевые слова');
-		$name = 'Alexandr';
-		$age = '33';
-		$names = ['Test', 'Yest'];
-		$cache = Cache::instance();
-		//$cache->set('test', $names);
-		$cache->delete('test');
-		$data = $cache->get('test');
-		if (!$data){
-			$cache->set('test', $names);
-		}
-		$this->set(compact('name', 'age', 'posts'));
+
 	}
 }
