a16e20cfb59cde422136ff3ef47bf2c2ab11bd01
Alexandr Shaposhnik <bromodragonfly11@gmail.com>
2022-03-30 18:30:15 +0300

    fix

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fd5c65c781161a37d62e373c8d222bd9cdf83d61',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fd5c65c781161a37d62e373c8d222bd9cdf83d61',
+            'dev_requirement' => false,
+        ),
+        'gabordemooij/redbean' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gabordemooij/redbean',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'd6da16f803a1850d83e6fef3735802675f8cbd5a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
