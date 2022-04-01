e21d63ce500c1b3ce9d339c0fd7bd1eba2127688
Alexandr Shaposhnik <bromodragonfly11@gmail.com>
2022-04-01 19:57:22 +0300

    add product page

--- app/views/layouts/watches.php
+++ app/views/layouts/watches.php
@@ -8,6 +8,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <html>
 <head>
     <?=$this->getMeta();?>
+	<base href="/">
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
     <!--Custom-Theme-files-->
     <!--theme-style-->
@@ -18,6 +19,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     <!--start-menu-->
     <link href="megamenu/css/style.css" rel="stylesheet" type="text/css" media="all" />
     <link href="megamenu/css/ionicons.min.css" rel="stylesheet" type="text/css" media="all" />
+	<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+
 </head>
 <body>
 <!--top-header-->
@@ -60,7 +63,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!--top-header-->
 <!--start-logo-->
 <div class="logo">
-    <a href="index.html"><h1>Luxury Watches</h1></a>
+    <a href="<?=PATH?>"><h1>Luxury Watches</h1></a>
 </div>
 <!--start-logo-->
 <!--bottom-header-->
@@ -295,7 +298,6 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!--footer-end-->
 <script src="js/jquery-1.11.0.min.js"></script>
 <script src="js/simpleCart.min.js"> </script>
-<script>$(document).ready(function(){$(".memenu").memenu();});</script>
 <!--dropdown-->
 <script src="js/jquery.easydropdown.js"></script>
 <!--Slider-Starts-Here-->
@@ -322,6 +324,41 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 </script>
 <script src="js/main.js"></script>
 <script src="megamenu/js/megamenu.js"></script>
+
+<script src="js/imagezoom.js"></script>
+<script defer src="js/jquery.flexslider.js"></script>
+<script>
+	// Can also be used with $(document).ready()
+	$(window).load(function() {
+		$('.flexslider').flexslider({
+			animation: "slide",
+			controlNav: "thumbnails"
+		});
+	});
+</script>
+<script src="js/jquery.easydropdown.js"></script>
+<script type="text/javascript">
+	$(function() {
+
+		var menu_ul = $('.menu_drop > li > ul'),
+			menu_a  = $('.menu_drop > li > a');
+
+		menu_ul.hide();
+
+		menu_a.click(function(e) {
+			e.preventDefault();
+			if(!$(this).hasClass('active')) {
+				menu_a.removeClass('active');
+				menu_ul.filter(':visible').slideUp('normal');
+				$(this).addClass('active').next().stop(true,true).slideDown('normal');
+			} else {
+				$(this).removeClass('active');
+				$(this).next().stop(true,true).slideUp('normal');
+			}
+		});
+
+	});
+</script>
 <!--End-slider-script-->
 </body>
 </html>
